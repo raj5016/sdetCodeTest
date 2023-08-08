44477e2e1e9e0d1e05cd44e4d2fef706de65594b
mgovindm5016 <mgovindarajm@gmail.com>
2023-08-08 08:12:34 +0530

    added hackrank phonebook

---
+++ src/main/java/mandatoryHomeWork/week6/CLW_1_PhoneBookProblem.java
@@ -0,0 +1,46 @@
+package mandatoryHomeWork.week6;
+
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.Scanner;
+import java.util.Set;
+
+public class CLW_1_PhoneBookProblem {
+	
+	//https://www.hackerrank.com/challenges/phone-book/problem?isFullScreen=true
+	
+	static HashMap<String,Integer> map=new HashMap<String,Integer>();
+
+	public static void main(String []argh)
+	{
+		Scanner in = new Scanner(System.in);
+		int n=in.nextInt();
+		in.nextLine();
+		
+		
+		for(int i=0;i<n;i++)
+		{
+			String name=in.nextLine();
+			int phone=in.nextInt();
+			in.nextLine();
+			map.put(name, phone);
+		}
+	
+		while(in.hasNext())
+		{	
+			String s=in.nextLine();
+			returnPersons(s);
+	}
+
+	}
+	public static void returnPersons(String inputQuery){
+		if(map.containsKey(inputQuery)) {
+			   Object value = map.get(inputQuery);
+	            System.out.println(inputQuery +"="+ value);
+		}else {
+				System.out.println("Not found");
+		}
+	}
+}
