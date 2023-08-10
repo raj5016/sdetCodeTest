524ed72c8b547d4d51de8e20c213e3e8ca583f58
mgovindm5016 <mgovindarajm@gmail.com>
2023-08-10 11:26:46 +0530

    Straight line slope

---
+++ src/main/java/mandatoryHomeWork/week6/HM3_CheckItsAStraightLine.java
@@ -0,0 +1,44 @@
+package mandatoryHomeWork.week6;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class HM3_CheckItsAStraightLine {
+	
+	//https://leetcode.com/problems/check-if-it-is-a-straight-line/description/
+	
+	//Reference -https://www.youtube.com/watch?v=q9RVkFXRM4s
+	
+	@Test
+	public void test() {
+		Assert.assertEquals(false, checkStaright(new int[] [] {{1,1},{2,2},{3,4},{4,5},{5,6},{7,7}}));
+	}
+	
+	@Test
+	public void test1() {
+		Assert.assertEquals(false, checkStaright(new int[] [] {{1,2},{2,3},{3,4}}));
+	}
+	
+	public boolean checkStaright(int[][] coordinates) 
+	{
+		int nums1[]=coordinates[0];
+		int nums2[]=coordinates[1];
+		
+		float slope=slope(nums1,nums2);
+		
+		for (int i = 2; i < coordinates.length; i++) {
+				int nums3[]=coordinates[i];
+				float newslope=slope(nums1,nums3);
+				
+				if(newslope!=slope) {
+					return false;
+				}
+		}
+		return true;
+		
+	}
+
+	float slope(int[] x, int[] y) {
+		return (float)(y[1]-x[1])/(y[0]-x[0]);
+	}
+}
