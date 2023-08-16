c5d63ce864a4f2c89e8c20d1701bf5cc1121caa6
mgovindm5016 <mgovindarajm@gmail.com>
2023-08-16 21:51:53 +0530

    adding the dayOfweek program

--- src/main/java/mandatoryHomeWork/week6/HM_4_DayOfWeek.java
+++ src/main/java/mandatoryHomeWork/week6/HM_4_DayOfWeek.java
@@ -1,7 +1,30 @@
 package mandatoryHomeWork.week6;
 
+import java.time.LocalDate;
+
+import org.junit.Assert;
+import org.junit.Test;
+
 public class HM_4_DayOfWeek {
 	
 	//https://leetcode.com/problems/day-of-the-week/
+	
+		 public String dayOfTheWeek(int day, int month, int year) {
+		        String[] days = {"Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"};
+		        //intDay has range from 1 to 7 start from Monday
+		        int intDay  = LocalDate.of(year,month,day).getDayOfWeek().getValue(); 
+		/*
+		 * The expression is checking if intDay is equal to 7. If it is, then it returns
+		 * days[0], which would typically correspond to the name of Sunday. If intDay is
+		 * not equal to 7, then it returns days[intDay], which corresponds to the name
+		 * of the day represented by the value of intDay.
+		 */
+		        return intDay==7?days[0]:days[intDay];
+		    }
+		 
+		 @Test
+		 public void test() {
+			 Assert.assertEquals("Saturday", dayOfTheWeek(31, 8, 2019));
+		 }
 
 }
