97ec95616ad871ad730f0afefe29391590cc40d2
mgovindm5016 <mgovindarajm@gmail.com>
2023-08-22 17:31:37 +0530

    Updated set-mismatch leetcode 645

---
+++ src/main/java/mandatoryHomeWork/week8/HM2_LeetCode_645_SetMismatch.java
@@ -0,0 +1,53 @@
+package mandatoryHomeWork.week8;
+
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class HM2_LeetCode_645_SetMismatch {
+		// https://leetcode.com/problems/set-mismatch/
+
+	@Test
+	public void test() {
+		Assert.assertArrayEquals(new int[] {2,3}, findErrorNums(new int[] {1,2,2,4}));
+	}
+	
+	@Test
+	public void test1() {
+		Assert.assertArrayEquals(new int[] {1,2}, findErrorNums(new int[] {1,1}));
+	}
+
+	@Test
+	public void test2() {
+		Assert.assertArrayEquals(new int[] {5,6}, findErrorNums(new int[] {1,2,3,4,5,5,7,8,9}));
+	}
+	
+	@Test
+	public void test3() {
+		Assert.assertArrayEquals(new int[] {2,1}, findErrorNums(new int[] {2,2}));
+	}
+	
+	
+	public int[] findErrorNums(int[] nums) {
+        int[] count= new int[nums.length + 1];
+        
+        for (int i = 0; i < nums.length; i++) {
+			count[nums[i]]++;  //1,1
+			//nums[0]=1
+			//count[1]=0 since we are incrementing , value 0 is changed to 1
+		}
+
+        int dup = 0, miss = 0;
+        
+        for (int i = 1; i < count.length; i++) {
+            if (count[i] == 2) {
+                dup = i;
+            }
+            if (count[i] == 0) {
+                miss = i;
+            }
+        }
+		return new int[] {dup,miss};
+	}
+
+}
