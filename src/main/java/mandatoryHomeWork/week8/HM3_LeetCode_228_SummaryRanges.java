3ba507facddf2e6ed715d7be9d22a6d61d3602f8
mgovindm5016 <mgovindarajm@gmail.com>
2023-08-24 21:13:31 +0530

    Commit

---
+++ src/main/java/mandatoryHomeWork/week8/HM3_LeetCode_228_SummaryRanges.java
@@ -0,0 +1,43 @@
+package mandatoryHomeWork.week8;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import org.junit.Test;
+import org.testng.Assert;
+
+public class HM3_LeetCode_228_SummaryRanges {
+	
+		@Test
+		public void test() {
+			Assert.assertEquals(Arrays.asList("0->2","4->5","7"), summaryRanges(new int[] {0,1,2,4,5,7}));
+		}
+	
+		public List<String> summaryRanges(int[] nums) {
+			List<String> list=new ArrayList<String>();
+			int fromIndex=0;
+			int toIndex=nums.length;
+			
+			
+			for (int i = 0; i <=nums.length-1; i++) {
+				fromIndex=i;
+				while(i!=nums.length-1 && nums[i]+1==nums[i+1]) {
+					i++;
+				}
+				toIndex=i;
+				if (fromIndex == toIndex) {
+	                list.add("" + nums[fromIndex]);
+
+	            } else {
+	            	list.add(nums[fromIndex] + "->" + nums[toIndex]);
+
+	            }
+
+	        }	            
+		
+		return list;
+        
+    }
+
+}
---
+++ src/test/java/mandatoryHomeWork/seleniumPractise/SalesForce_day4.java
@@ -0,0 +1,33 @@
+package mandatoryHomeWork.seleniumPractise;
+
+import java.time.Duration;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class SalesForce_day4 {
+	WebDriver driver;
+	@Test
+	public void findDriverName() {
+		driver=new ChromeDriver();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
+		driver.manage().window().maximize();
+		driver.get("https://login.salesforce.com/");
+		driver.findElement(By.id("username")).sendKeys("hari.radhakrishnan@qeagle.com");
+		driver.findElement(By.id("password")).sendKeys("Leaf$1234");
+		driver.findElement(By.id("Login")).click();
+		driver.findElement(By.xpath("//div[@class='slds-icon-waffle']"));
+		driver.findElement(By.xpath("//button[text()='View All']")).click();
+		driver.findElement(By.id("//p[text()='Sales']//following::p[text()='Manage your sales process with accounts, leads, opportunities,...']")).click();
+		driver.findElement(By.id("//p[text()='Accounts']")).click();
+		driver.findElement(By.id("//div[text()='New']")).click();
+		driver.findElement(By.xpath("//label[text()='Account Name']//following::div[1]/input")).sendKeys("Rah78");
+		driver.findElement(By.id("//button[text()='Save']")).click();
+		driver.close();
+	}
+	}
+
+
+
+
