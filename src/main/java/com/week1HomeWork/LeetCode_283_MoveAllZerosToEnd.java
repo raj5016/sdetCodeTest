03acc3c4629b47ede3085d99198a87aa4045c70d
raj5016 <govindaraj0508@gmail.com>
2023-10-02 12:41:00 +0530

    Updated id

--- src/main/java/com/week1HomeWork/LeetCode_283_MoveAllZerosToEnd.java
+++ src/main/java/com/week1HomeWork/LeetCode_283_MoveAllZerosToEnd.java
@@ -1,16 +1,12 @@
 package com.week1HomeWork;
 
-import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.List;
 
 import org.junit.Test;
 
-import junit.framework.Assert;
+public class LeetCode_283_MoveAllZerosToEnd {
 
-public class MoveAllZerosToEnd {
-
-	/*
+	/*	https://leetcode.com/problems/move-zeroes/description/
 		 Given an integer array nums, move all 0's to the end of it while maintaining the relative order of the non-zero elements.
 		
 		Note that you must do this in-place without making a copy of the array.
