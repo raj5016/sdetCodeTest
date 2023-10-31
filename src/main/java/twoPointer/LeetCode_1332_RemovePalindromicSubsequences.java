655ef075befd7af23f5bbd932cba887f193128a2
raj5016 <govindaraj0508@gmail.com>
2023-10-31 07:44:05 +0530

    comment updated

--- src/main/java/twoPointer/LeetCode_1332_RemovePalindromicSubsequences.java
+++ src/main/java/twoPointer/LeetCode_1332_RemovePalindromicSubsequences.java
@@ -13,7 +13,8 @@ public class LeetCode_1332_RemovePalindromicSubsequences {
 	
 	
 	public int removePalindromeSub(String s) {
-	    // answer can only be at most 2 because obviously you can remove all 'a's from the string and then remove all 'b's, that's 2 steps
+	    // answer can only be at most 2 because obviously you can remove all 'a's (1) from the string 
+		//and then remove all 'b's,(2) that's 2 steps
 	    // or you can just do it in 1 step if the whole string is palindrome
 	    return isPalindrome(s) ? 1 : 2;
 	  }
